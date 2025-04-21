9ff29bccaa9f03cc91e853c6f6f4e56091a86c41
Neha jaiswal <nj2443571@gmail.com>
2025-04-21 21:10:31 +0530

    Add files via upload

---
+++ even or add.py
@@ -0,0 +1,20 @@
+#check whether a number entered by user is even or odd
+'''n=int(input("enter a number\n"))
+if n%2==0:
+    print("number is even\n")
+else:
+    print("number is odd\n")'''
+
+    #table
+# ch=int(input("enter ur no:"))
+# for i in range (1,11):
+#     sum=ch*i
+#     print(ch,"x",i,"=",sum)
+
+sum=0
+n=int(input("enter s num"))
+for i in range(1,n+1,1):
+    sum+=i
+print(sum)
+
+
